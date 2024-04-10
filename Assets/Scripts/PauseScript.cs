cc06a6f35f972f5fadfacff317cab777ba6b7c37
Ortem08 <72299038+Ortem08@users.noreply.github.com>
2024-04-10 12:24:05 +0500

    Вырубил движение камеры во время паузы, подкрутил панель на весь экран

--- Assets/Scripts/PauseScript.cs
+++ Assets/Scripts/PauseScript.cs
@@ -7,6 +7,9 @@ public class PauseScript : MonoBehaviour
 {
     public static bool GameIsPaused;
     public GameObject PauseMenuUI;
+    
+    //Объект игрока, чтобы во время паузы не реагировал на мышь
+    public GameObject player;
 
     private void Update()
     {
@@ -21,14 +24,18 @@ public class PauseScript : MonoBehaviour
 
     public void Resume()
     {
-        PauseMenuUI.SetActive(false);
+        PauseMenuUI.SetActive(false); 
+        player.GetComponent<FirstPersonController>().enabled = true;
         Time.timeScale = 1f;
         GameIsPaused = false;
+
+
     }
 
     void Pause()
     {
-        PauseMenuUI.SetActive(true);
+        PauseMenuUI.SetActive(true); 
+        player.GetComponent<FirstPersonController>().enabled = false;
         Time.timeScale = 0f;
         GameIsPaused = true;
     }
