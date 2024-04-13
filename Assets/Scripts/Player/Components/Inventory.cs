e4a4bf2ab71b1e06c01f279a1e792f73c9cbc5f2
Evgeny Lapshin <zhenia.lapshin201@yandex.ru>
2024-04-13 23:24:16 +0500

    adding base player inventory

---
+++ Assets/Scripts/Player/Components/Inventory.cs
@@ -0,0 +1,11 @@
+﻿
+using System.Collections.Generic;
+
+public class Inventory
+{
+    private List<IInventoriable> items;
+
+    public void Add(IInventoriable item) => items.Add(item);
+
+    public void Remove(IInventoriable item) => items.Remove(item);
+}
---
+++ Assets/Scripts/Player/PlayerLogic.cs
@@ -0,0 +1,19 @@
+﻿
+using UnityEngine;
+
+public class PlayerLogic : MonoBehaviour
+{
+    public int Health { get; set; }
+    public Inventory Inventory { get; private set; }
+    public IInventoriable SelectedItem { get; set; }
+
+    private void Start()
+    {
+        Inventory = GetComponent<Inventory>();
+    }
+
+    private void Update()
+    {
+        SelectedItem?.Update();
+    }
+}
