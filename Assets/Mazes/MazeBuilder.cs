74396c46e164b0e60a4678b19630babedf79bbae
Alexey_Dzhevello <fishwere@mail.ru>
2024-04-14 13:58:00 +0500

    stable generation achieved

--- Assets/Mazes/MazeBuilder.cs
+++ Assets/Mazes/MazeBuilder.cs
@@ -4,38 +4,40 @@ using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Unity.VisualScripting;
 using UnityEngine;
 
 namespace Assets.Mazes
 {
     internal class MazeBuilder: MonoBehaviour
     {
+
         public void Start()
         {
             var generator = new MazePatternGenerator();
             var maze = generator.RandomizedDFS(10, 10);
-            using (var sw = new StreamWriter(@"C:\Users\Alex\Desktop\mazeGen.txt"))
+            /*using (var sw = new StreamWriter(@"C:\Users\Alex\Desktop\mazeGen.txt"))
             {
                 sw.Write(generator.StringifyPattern(maze));
-            }
-            
+            }*/
 
-            var curPos = transform.position;
+            var mazeGameObject = new GameObject("The Maze");
+            mazeGameObject.transform.position = transform.position;
+
             for (int i = 0; i < maze.GetLength(0); i++)
             {
-                curPos = transform.position + Vector3.right * i;
                 for (int j = 0; j < maze.GetLength(1); j++)
                 {
                     if (maze[i, j] == 0)
                     {
                         var go = GameObject.CreatePrimitive(PrimitiveType.Cube);
-                        go.transform.position = curPos;
+                        go.transform.position = mazeGameObject.transform.position + new Vector3(i, 0, j);
+                        go.transform.parent = mazeGameObject.transform;
                     }
-                    curPos += Vector3.forward;
                 }
             }
+
+            Destroy(gameObject);    //remove mazeBuilder from scene
         }
-
-
     }
 }
