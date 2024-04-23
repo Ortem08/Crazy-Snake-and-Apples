57a349b67a6cd64f2eb096398a7a013607253a49
Evgeny Lapshin <zhenia.lapshin201@yandex.ru>
2024-04-23 09:55:41 +0500

    adding randomly patrolling and chasing player

---
+++ Assets/Scripts/Snake/Movement/AINavigation.cs
@@ -0,0 +1,61 @@
+﻿
+
+using UnityEngine;
+using UnityEngine.AI;
+using Random = UnityEngine.Random;
+
+public class AINavigation : MonoBehaviour
+{
+    [SerializeField] private float randomPointRadius = 50;
+    [SerializeField] private Transform player;
+    [SerializeField] private float fieldOfViewAngle;
+    [SerializeField] private LayerMask obstructionMask;
+    private NavMeshAgent agent;
+    private NavMeshPath path;
+
+    private void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+        path = new NavMeshPath();
+    }
+
+    private void Update()
+    {
+        UpdateDestination();
+    }
+
+    private void UpdateDestination()
+    {
+        if (CheckCanSeePlayer())
+        {
+            agent.SetDestination(player.position);
+        }
+        if (!agent.hasPath)
+            TrySetNewDestination();
+    }
+
+    private bool CheckCanSeePlayer()
+    {
+        var directionToTarget = (player.position - transform.position).normalized;
+        if (Vector3.Angle(transform.forward, directionToTarget) < fieldOfViewAngle / 2)
+        {
+            var distanceToTarget = Vector3.Distance(transform.position, player.position);
+            if (!Physics.Raycast(transform.position, directionToTarget, distanceToTarget, obstructionMask))
+            {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    private void TrySetNewDestination()
+    {
+        var randomPoint = transform.position + Random.insideUnitSphere * randomPointRadius;
+        NavMesh.SamplePosition(randomPoint, out var hit, randomPointRadius, 1);
+        var destination = hit.position;
+        agent.CalculatePath(destination, path);
+        if (path.status == NavMeshPathStatus.PathComplete)
+            agent.SetPath(path);
+    }
+}
