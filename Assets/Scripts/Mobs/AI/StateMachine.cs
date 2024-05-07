995c4fa0360139cd24a15302d32f674df4912650
Ortem08 <72299038+Ortem08@users.noreply.github.com>
2024-05-07 18:47:33 +0500

    bower shit

--- Assets/Scripts/Mobs/AI/StateMachine.cs
+++ Assets/Scripts/Mobs/AI/StateMachine.cs
@@ -1,10 +1,15 @@
+using UnityEngine;
+
 public class StateMachine
 {
     public IState currentState;
 
+    public bool StateBrandNew { get; set; } = true;
+
     public void ChangeState(IState newState)
     {
         currentState = newState;
+        StateBrandNew = true;
     }
 
     public void Update()
