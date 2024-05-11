9013d3467034b7b890d8aa2359404a49da179212
ITSigma <ascoolasanton@gmail.com>
2024-05-11 22:27:25 +0500

    Clear Drop Handler

--- Assets/Scripts/UI/DropHandler.cs
+++ Assets/Scripts/UI/DropHandler.cs
@@ -9,7 +9,6 @@ using UnityEngine.UI;
 public class DropHandler : MonoBehaviour, IDropHandler
 {
     private GameObject cardChildren;
-    // private static Dictionary<GameObject, Image> itemImages = new Dictionary<GameObject, Image>();
     public UnityEvent<GameObject, GameObject> OnSwap { get; } = new();
 
 
@@ -20,21 +19,6 @@ public class DropHandler : MonoBehaviour, IDropHandler
 
     public void OnDrop(PointerEventData eventData)
     {
-        // var tempImage = itemImage?.sprite;
-        // if (itemImages.TryGetValue(eventData.pointerDrag, out var image))
-        // {
-        //     itemImage.sprite = itemImages[eventData.pointerDrag]?.sprite;
-        // }
-        // else
-        // {
-        //     itemImage.sprite = null;
-        // }
-        // itemImages[eventData.pointerDrag].sprite = tempImage;
-        
         OnSwap.Invoke(eventData.pointerDrag, cardChildren);
-        
-        // var otherItemTransform = eventData.pointerDrag.transform;
-        // otherItemTransform.SetParent(transform);
-        // otherItemTransform.position = transform.position; 
     }
 }
